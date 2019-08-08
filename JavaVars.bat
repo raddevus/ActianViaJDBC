ecde65e7ed06e24096b847643d94e45fe8119d73
raddevus <1radeutsch1@gmail.com>
2019-08-08 14:29:56 -0400

    Altered paths in javavars.bat to remind others to point them to paths on their systems

--- JavaVars.bat
+++ JavaVars.bat
@@ -1,6 +1,7 @@
+rem ### CHANGE ALL PATHS SO THEY POINT TO VALID DIRECTORIES ON YOUR SYSTEM  ####
 set JDK_HOME=C:\Program Files\Java\jdk1.8.0_161
 set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_161
-set CLASSPATH=%CLASSPATH%;C:\Users\roger.deutsch\Dev\Actian\PSQL-SDK-JDBC-13.30.034.000\bin\pvjdbc2.jar;
-set CLASSPATH=%CLASSPATH%;C:\Users\roger.deutsch\Dev\Actian\PSQL-SDK-JDBC-13.30.034.000\bin\pvjdbc2x.jar;
-set CLASSPATH=%CLASSPATH%;C:\Users\roger.deutsch\Dev\Actian\PSQL-SDK-JDBC-13.30.034.000\bin\jpscs.jar;
+set CLASSPATH=%CLASSPATH%;<YOUR_PATH_TO_JARS>\Actian\PSQL-SDK-JDBC-13.30.034.000\bin\pvjdbc2.jar;
+set CLASSPATH=%CLASSPATH%;<YOUR_PATH_TO_JARS>\Actian\PSQL-SDK-JDBC-13.30.034.000\bin\pvjdbc2x.jar;
+set CLASSPATH=%CLASSPATH%;<YOUR_PATH_TO_JARS>\Actian\PSQL-SDK-JDBC-13.30.034.000\bin\jpscs.jar;
 path=%path%;C:\Program Files\Java\jdk1.8.0_161\bin;
